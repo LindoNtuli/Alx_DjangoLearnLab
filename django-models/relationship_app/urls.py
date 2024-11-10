6fe7ee7fb154ca1d901aafd098c5d08cd0de65c9
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-11-10 16:11:27 +0200

    ALx_DjangoLearnLab

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+from django.urls import path, include
+
+urlpatterns = [
+    path('relationship/', include('relationship_app.urls')),
+    # other URL patterns...
+]
