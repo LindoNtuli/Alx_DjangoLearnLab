198b110161948887f87666a9d5ac021a89209486
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-11-17 22:29:41 +0200

    django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 from .views import register, user_login, user_logout
-from .views import admin_view, librarian_view, member_view
+from .views import admin_view
+from .views import librarian_view
+from .views import member_view
 from .views import add_book, edit_book, delete_book
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
@@ -15,7 +17,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('admin/', admin_view, name='admin_view'),
+    path('admin/', admin_view, name='admin_dashboard'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
     path('books/add/', add_book, name='add_book'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Library, Book
@@ -38,11 +38,12 @@ def delete_book(request, book_id):
 
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and user.groups.filter(name='Admin').exists()
 
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'admin_dashboard.html')
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
