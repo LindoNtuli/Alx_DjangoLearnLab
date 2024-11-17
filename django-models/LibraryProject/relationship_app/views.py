858631982ba6648d84c859a201fd6037a1415fbd
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-11-17 22:48:08 +0200

    django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -38,12 +38,12 @@ def delete_book(request, book_id):
 
 
 def is_admin(user):
-    return user.is_authenticated and user.groups.filter(name='Admin').exists()
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
 @login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_dashboard.html')
+    return render(request, 'admin_template.html', {})
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
