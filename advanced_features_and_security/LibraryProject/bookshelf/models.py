f94d1d52084d4215473a2ba59542fce797ea05a5
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-11-24 23:36:44 +0200

    LibraryProject

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import User
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view article"),
+            ("can_create", "Can create article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
