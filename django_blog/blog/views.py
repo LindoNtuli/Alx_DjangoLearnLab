ffaa81580c6ec601a0d94a6a0b837367b5a3d959
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-12-08 11:21:13 +0200

    ALx_DjangoLearnLab

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'
@@ -22,3 +27,43 @@ def register(request):
 
 class CustomLogoutView(LogoutView):
     next_page = '/'
+#end
+    
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+#end
