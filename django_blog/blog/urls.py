013b9e0b1c7d3f85c220a66cddfb341fa5681a36
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-12-08 21:34:38 +0200

    ALx_DjangoLearnLab

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:post_id>/', post_detail, name='post_detail'),
-    path('comments/new/', CommentCreateView.as_view(), name='comment_create'),
-    path('comment/update/<pk>/', CommentUpdateView.as_view(), name='comment_update'),
-    path('comment/delete/<pk>/', CommentDeleteView.as_view(), name='comment_delete'),
+    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
