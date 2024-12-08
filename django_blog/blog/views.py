c5ed158dd7345cbf90382f3f2ce8a1a50f7761d9
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-12-08 11:09:05 +0200

    ALx_DjangoLearnLab

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,46 +1,24 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-from django.contrib.auth import logout
-from django.contrib import messages
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
 
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('profile')
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('profile')
-        else:
-            messages.error(request, 'Invalid username or password.')
-    return render(request, 'login.html')
-
-def user_logout(request):
-    logout(request)
-    return redirect('login')
-
-@login_required
-def profile(request):
-    return render(request, 'profile.html', {'user': request.user})
-
-@login_required
-def edit_profile(request):
-    if request.method == 'POST':
-        user = request.user
-        user.email = request.POST['email']
-        user.save()
-        return redirect('profile')
-    return render(request, 'edit_profile.html', {'user': request.user})
+class CustomLogoutView(LogoutView):
+    next_page = '/'
