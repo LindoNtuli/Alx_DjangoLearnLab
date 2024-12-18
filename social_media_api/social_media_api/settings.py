924f3c58d79cbe5431574cb787ec6f395d32f1c1
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-12-18 14:42:58 +0200

    social_media_api

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r1=occ0wqdxsn@1uy2122n909@uj^mp&9fscimkf&8s1p9xtzi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.example.com', 'my_server_ip', 'localhost']
 
 
 # Application definition
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lindo',
+        'USER': 'lindo',
+        'PASSWORD': 'lili',
+        'HOST': 'localhost',         # Set to 'localhost'
+        'PORT': '5432',              # Default port for PostgreSQL
     }
 }
 
@@ -132,5 +136,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True  # Use this if you have SSL configured
 
 
