1b6c979abba7115b13e20655b43673e92a40ba23
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-12-18 13:18:02 +0200

    social_media_api

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,6 +6,6 @@ from .views import FollowViewSet
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('follow/<int:pk>/', UserViewSet.as_view({'post': 'follow_user'}), name='follow_user'),
-    path('unfollow/<int:pk>/', UserViewSet.as_view({'post': 'unfollow_user'}), name='unfollow_user'),
+    path('follow/<int:user_id>/', UserViewSet.as_view({'post': 'follow_user'}), name='follow_user'),
+    path('unfollow/<int:user_id>/', UserViewSet.as_view({'post': 'unfollow_user'}), name='unfollow_user'),
 ]
